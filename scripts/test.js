ca48eb2f517e49fd5419c01bccdacd2c608e9626
Florian Maunier <fmaunier@gmail.com>
2016-11-30 15:30:21 +0100

    Preparing project architecture

---
+++ scripts/test.js
@@ -0,0 +1,11 @@
+import Jasmine from "jasmine";
+import {JUnitXmlReporter, TapReporter} from "jasmine-reporters";
+
+var jasmine = new Jasmine();
+
+jasmine.env.clearReporters();
+jasmine.addReporter(new JUnitXmlReporter());
+jasmine.addReporter(new TapReporter());
+
+jasmine.loadConfigFile('test/support/jasmine.json');
+jasmine.execute();
