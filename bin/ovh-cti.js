c90a15a335bf5ea9dc40bdb25ac4d6bc9de6f4e4
Florian Maunier <fmaunier@gmail.com>
2016-12-06 18:12:11 +0100

    Line Status Middleware is ready

--- bin/ovh-cti.js
+++ bin/ovh-cti.js
@@ -1,6 +1,7 @@
 const Configstore = require('configstore');
 const OvhCti = require('../lib/index.js');
 const dropRegisteredEventsMiddleware = require('../lib/middlewares/drop-registered-events-middleware');
+const lineStatusMiddleware = require('../lib/middlewares/line-status-middleware');
 const mqttHandler = require('../lib/handlers/mqtt-handler');
 const influxHandler = require('../lib/handlers/influx-handler');
 const pkg = require('../package.json');
@@ -12,6 +13,7 @@ const token = conf.get('token');
 if (/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/.test(token)) {
   const cti = new OvhCti(token);
   cti.addMiddleware(dropRegisteredEventsMiddleware);
+  cti.addMiddleware(lineStatusMiddleware);
   cti.addHandler(mqttHandler(conf));
   cti.addHandler(influxHandler(conf));
   cti.run();
--- src/middlewares/line-status-middleware.js
+++ src/middlewares/line-status-middleware.js
@@ -1,14 +1,39 @@
 import dbg from 'debug';
 
 const debug = dbg('ovh-cti:middlewares:line-status');
+
 const lineStatus = {};
+const activeCalls = {};
 
 export default function (event) {
   return new Promise(resolve => {
     event.Events.map(evt => {
-      lineStatus[evt.Ressource] = evt.Event;
+      let startDate = new Date(evt.Data.Ts * 1000);
+      startDate.setTime(startDate.getTime() - startDate.getTimezoneOffset() * 60 * 1000);
+      evt.Data.DateStart = startDate;
+
+      switch (evt.Event) {
+        case 'start_ringing':
+          lineStatus[evt.Ressource] = 'ringing';
+          activeCalls[evt.Data.CallId] = evt.Data;
+          break;
+        case 'start_calling':
+          lineStatus[evt.Ressource] = 'calling';
+          if (!evt.Data.CallId in activeCalls) {
+            activeCalls[evt.Data.CallId] = evt.Data;
+          }
+          break;
+        case 'end_ringing':
+        case 'end_calling':
+          lineStatus[evt.Ressource] = 'idle';
+          if (evt.Data.CallId in activeCalls) {
+            delete activeCalls[evt.Data.CallId];
+          }
+          break;
+      }
     });
-    debug('');
+    debug('Lines: ', lineStatus);
+    debug('Calls: ', activeCalls)
     resolve(event);
   });
 }
