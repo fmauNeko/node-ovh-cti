a1f3aeea7234aa263f2a32e07f65c431454849ba
Florian Maunier <fmaunier@gmail.com>
2017-05-16 11:14:29 +0200

    Fix XO crying

--- src/middlewares/line-status-middleware.js
+++ src/middlewares/line-status-middleware.js
@@ -14,18 +14,21 @@ export default function (event) {
 
       switch (evt.Event) {
         case 'start_ringing':
-          evt.Status = lineStatus[evt.Ressource] = 'ringing';
+          evt.Status = 'ringing';
+          lineStatus[evt.Ressource] = 'ringing';
           activeCalls[evt.Data.CallId] = evt.Data;
           break;
         case 'start_calling':
-          evt.Status = lineStatus[evt.Ressource] = 'calling';
+          evt.Status = 'calling';
+          lineStatus[evt.Ressource] = 'calling';
           if (!(evt.Data.CallId in activeCalls)) {
             activeCalls[evt.Data.CallId] = evt.Data;
           }
           break;
         case 'end_ringing':
         case 'end_calling':
-          evt.Status = lineStatus[evt.Ressource] = 'idle';
+          evt.Status = 'idle';
+          lineStatus[evt.Ressource] = 'idle';
           if (evt.Data.CallId in activeCalls) {
             evt.CallDuration = (evt.Data.DateStart - activeCalls[evt.Data.CallId].DateStart) / 1000;
             delete activeCalls[evt.Data.CallId];
